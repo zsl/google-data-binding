c64ae35dc1f125186351cb75dc0e11a0071e934c
Yigit Boyar <yboyar@google.com>
2015-04-21 14:05:41 -0700

    WIP eap

--- samples/BindingDemo/app/src/main/java/com/android/example/bindingdemo/vo/Users.java
+++ samples/BindingDemo/app/src/main/java/com/android/example/bindingdemo/vo/Users.java
@@ -3,6 +3,25 @@ package com.android.example.bindingdemo.vo;
 import com.android.example.bindingdemo.R;
 
 public class Users {
+    public static final int[] ICONS = {
+            R.drawable.emo_im_angel,
+            R.drawable.emo_im_cool,
+            R.drawable.emo_im_crying,
+            R.drawable.emo_im_embarrassed,
+            R.drawable.emo_im_foot_in_mouth,
+            R.drawable.emo_im_happy,
+            R.drawable.emo_im_kissing,
+            R.drawable.emo_im_laughing,
+            R.drawable.emo_im_lips_are_sealed,
+            R.drawable.emo_im_money_mouth,
+            R.drawable.emo_im_sad,
+            R.drawable.emo_im_surprised,
+            R.drawable.emo_im_tongue_sticking_out,
+            R.drawable.emo_im_undecided,
+            R.drawable.emo_im_winking,
+            R.drawable.emo_im_wtf,
+            R.drawable.emo_im_yelling,
+    };
     public static final User[] robots = new User[]{
             new User("romain", "guy", R.drawable.romain, User.ROBOT),
     };
