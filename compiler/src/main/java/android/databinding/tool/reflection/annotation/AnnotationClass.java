23ad7265de1729abdcda4fc3dbae5f7e6818c6ac
George Mount <mount@google.com>
2015-08-24 13:55:12 -0700

    Fix access to static field of generic object.
    Bug 23491360
    
    Change-Id: I94c62b67dfa8f8bd4c7f9fb5ab1adff4a927f77b

--- compiler/src/main/java/android/databinding/tool/reflection/annotation/AnnotationClass.java
+++ compiler/src/main/java/android/databinding/tool/reflection/annotation/AnnotationClass.java
@@ -15,8 +15,6 @@
  */
 package android.databinding.tool.reflection.annotation;
 
-import org.antlr.v4.codegen.model.decl.Decl;
-
 import android.databinding.tool.reflection.ModelAnalyzer;
 import android.databinding.tool.reflection.ModelClass;
 import android.databinding.tool.reflection.ModelField;
@@ -57,7 +55,25 @@ class AnnotationClass extends ModelClass {
 
     @Override
     public String toJavaCode() {
+        if (isIncomplete(mTypeMirror)) {
+            return getCanonicalName();
+        }
         return mTypeMirror.toString();
+    }
+
+    private static boolean isIncomplete(TypeMirror typeMirror) {
+        final TypeKind typeKind = typeMirror.getKind();
+        if (typeKind == TypeKind.DECLARED) {
+            DeclaredType declaredType = (DeclaredType) typeMirror;
+            for (TypeMirror typeArg : declaredType.getTypeArguments()) {
+                if (isIncomplete(typeArg)) {
+                    return true;
+                }
+            }
+        } else if (typeKind == TypeKind.TYPEVAR) {
+            return true;
+        }
+        return false;
     }
 
     @Override
