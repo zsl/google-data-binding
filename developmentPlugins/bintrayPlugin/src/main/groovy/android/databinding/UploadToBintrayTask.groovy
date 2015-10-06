90d6351746810033df84e2e6878584d64ebae098
Yigit Boyar <yboyar@google.com>
2015-10-06 11:12:28 -0700

    Ignore warnings from bintray
    Change-Id: Id241077e22bef6000ae13c06c8538dd59a5293c8

--- developmentPlugins/bintrayPlugin/src/main/groovy/android/databinding/UploadToBintrayTask.groovy
+++ developmentPlugins/bintrayPlugin/src/main/groovy/android/databinding/UploadToBintrayTask.groovy
@@ -74,9 +74,9 @@ public class UploadToBintrayTask extends DefaultTask {
             }
             def responseText = execute.text
             def json = new JsonSlurper().parseText(responseText)
-            if (json.getAt("message") != "success") {
+            if (json.getAt("message") != "success" && json.getAt("message") != null) {
                 throw new RuntimeException("Cannot upload artifact. Error response: " +
-                        "${json.getAt("message")}")
+                        "${json.getAt("message")} ${responseText}")
             }
             println("uploaded $localFile")
         }
