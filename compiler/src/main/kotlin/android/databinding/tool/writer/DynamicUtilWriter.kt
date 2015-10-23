71d2287133f4c2d6afde979ef11fb047b7d47372
George Mount <mount@google.com>
2015-10-23 14:02:39 -0700

    Add targetSdk check for DynamicUtil.
    Change-Id: I271b3fc364c2d8689f22e0555174a8ca30bc04b5
    (cherry picked from commit 83a8c9d56cba07985d611ce7df47346b94742336)

--- compiler/src/main/kotlin/android/databinding/tool/writer/DynamicUtilWriter.kt
+++ compiler/src/main/kotlin/android/databinding/tool/writer/DynamicUtilWriter.kt
@@ -1,36 +1,39 @@
-package android.databinding.tool.writer
-
+package android.databinding.tool.writer;
 
 class DynamicUtilWriter() {
-    public fun write() : String {
-        return """
-package android.databinding;
-
-import android.content.res.ColorStateList;
-import android.graphics.drawable.Drawable;
-import android.os.Build.VERSION;
-import android.os.Build.VERSION_CODES;
-import android.view.View;
-
-public class DynamicUtil {
-    @SuppressWarnings("deprecation")
-    public static ColorStateList getColorStateListFromResource(View root, int resourceId) {
-        if (VERSION.SDK_INT >= VERSION_CODES.M) {
-            return root.getContext().getColorStateList(resourceId);
-        } else {
-            return root.getResources().getColorStateList(resourceId);
+    public fun write(targetSdk : kotlin.Int) : KCode = kcode("package android.databinding;") {
+        nl("")
+        nl("import android.content.res.ColorStateList;")
+        nl("import android.graphics.drawable.Drawable;")
+        nl("import android.os.Build.VERSION;")
+        nl("import android.os.Build.VERSION_CODES;")
+        nl("import android.view.View;")
+        nl("")
+        nl("public class DynamicUtil {")
+        tab("@SuppressWarnings(\"deprecation\")") {
+            nl("public static ColorStateList getColorStateListFromResource(View root, int resourceId) {")
+            if (targetSdk >= 23) {
+                tab("if (VERSION.SDK_INT >= VERSION_CODES.M) {") {
+                    tab("return root.getContext().getColorStateList(resourceId);")
+                }
+                tab("}")
+            }
+            tab("return root.getResources().getColorStateList(resourceId);")
         }
-    }
+        tab("}")
+        nl("")
 
-    @SuppressWarnings("deprecation")
-    public static Drawable getDrawableFromResource(View root, int resourceId) {
-        if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
-            return root.getContext().getDrawable(resourceId);
-        } else {
-            return root.getResources().getDrawable(resourceId);
+        tab("@SuppressWarnings(\"deprecation\")") {
+            nl("public static Drawable getDrawableFromResource(View root, int resourceId) {")
+            if (targetSdk >= 21) {
+                tab("if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {") {
+                    tab("return root.getContext().getDrawable(resourceId);")
+                }
+                tab("}")
+            }
+            tab("return root.getResources().getDrawable(resourceId);")
         }
-    }
-}
-"""
+        tab("}")
+        nl("}")
     }
 }
