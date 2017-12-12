29efe49544b50b68b29b6a9bf99f50555d551843
Yigit Boyar <yboyar@google.com>
2017-12-12 06:01:25 +0000

    Revert "Do not include package name twice in the name of generated files"
    This reverts commit 252fee6b12d9b69176c007f54ab060f96e6c679e.
    
    Reason for revert: The change broke blaze integration.
    
    Change-Id: I5e6bc9fb8b02085acae8d32119dd64cc9f912cf3

--- compiler/src/main/java/android/databinding/annotationprocessor/ProcessBindable.java
+++ compiler/src/main/java/android/databinding/annotationprocessor/ProcessBindable.java
@@ -88,7 +88,7 @@ public class ProcessBindable extends ProcessDataBinding.ProcessingStep implement
                 }
             }
             GenerationalClassUtil.writeIntermediateFile(mProperties.getPackage(),
-                    GenerationalClassUtil.ExtensionFilter.BR.getExtension(), mProperties);
+                    createIntermediateFileName(mProperties.getPackage()), mProperties);
             generateBRClasses(args, mProperties.getPackage());
         }
         return false;
@@ -107,6 +107,10 @@ public class ProcessBindable extends ProcessDataBinding.ProcessingStep implement
     @Override
     public void onProcessingOver(RoundEnvironment roundEnvironment,
             ProcessingEnvironment processingEnvironment, DataBindingCompilerArgs args) {
+    }
+
+    private String createIntermediateFileName(String appPkg) {
+        return appPkg + GenerationalClassUtil.ExtensionFilter.BR.getExtension();
     }
 
     private void generateBRClasses(DataBindingCompilerArgs compilerArgs, String pkg) {
